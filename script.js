f905924a4eb947f23cb4e06433edbc20652147a5
Collince2021 <okotocollince@gmail.com>
2022-08-27 00:20:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+$(function (){
+$(".para").click(
+  function (){
+  var $find=$(this).find(".hide-para");
+  if ($find.hasClass("open")){
+   $find.removeClass("open");
+   $find.slideDown();
+   $(this).find(".sign").text("-");}
+    else {
+      $find.addClass("open");
+      $find.slideUp();
+       $(this).find(".sign").text("+");
+      
 
+
+}
+  
+})
+
+
+
+})
