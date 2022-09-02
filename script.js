be019228788af1f078cdb0d63e16e34c2288c70c
Collince2021 <okotocollince@gmail.com>
2022-09-02 22:08:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ var $finding=$(this).find(".hide-para");
                   if($finding.hasClass("open") ){
                        $finding.removeClass("open");
                        $finding.fadeOut();
-                       $(this).children().hasClass(".sign").text("+");
+                       $(this).contents().hasClass(".sign").text("+");
                    
                                       }
                      else{
-                    $(this).children().hasClass(".sign").text("-");
+                    $(this).contents().hasClass(".sign").text("-");
                     $finding.addClass("open");
                     $finding.fadeIn();
                    
