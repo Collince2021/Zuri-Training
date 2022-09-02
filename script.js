56c146e4b7953f518cdfa5b277e35bc27184d0ee
Collince2021 <okotocollince@gmail.com>
2022-09-02 21:06:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ $(".two-ctn").click(function(){
 
 var $finding=$(this).find(".hide-para");
                   if($finding.hasClass("open") ){
-                    $(".sign").text("-");
-                    $finding.removeClass("open");
-                    $finding.fadeOut();
+                       $finding.removeClass("open");
+                       $finding.fadeOut();
+                       $(".sign").text("+");
                    
                                       }
                      else{
-                    $(".sign").text("+");
+                    $(".sign").text("-");
                     $finding.addClass("open");
                     $finding.fadeIn();
                    
