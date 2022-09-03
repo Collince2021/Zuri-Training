107079bcc65008c81e1b10ebff331e21eb7031d7
Collince Okoto <okotocollince@gmail.com>
2022-09-03 06:51:39 +0000

    update style.css

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-$(function(){
+$(function() {
 
-$(".two-ctn").click(function(){
+  $(".two-ctn").click(function() {
 
-var $finding=$(this).find(".hide-para");
-                  if($finding.hasClass("open") ){
-                       $finding.removeClass("open");
-                       $finding.fadeOut();
-                       $(this).contents().hasClass("sign").text("+");
-                   
-                                      }
-                     else{
-                    $(this).contents().hasClass("sign").text("-");
-                    $finding.addClass("open");
-                    $finding.fadeIn();
-                        }
+    var $finding = $(this).find(".hide-para");
+    if ($finding.hasClass("open")) {
+      $finding.removeClass("open");
+      $finding.fadeOut();
+      $(this).find(".sign").css({
+        "transform": "none",
+        "color": "black",
+        "top": "-70px"
 
-})
+      })
+    }
+    else {
+      $(this).find(".sign").css({
+        "transform": "rotate(-30deg)",
+        "color": "red",
+        "top": "-120px",
+        "left": "340px"
+      });
+      $finding.addClass("open");
+      $finding.fadeIn();
+    }
+
+  })
 
 })
 
